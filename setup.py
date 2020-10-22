750eddd3cf2c539f6534cd9a80374a571ee14342
Jeremy Weinstein <jeremydw@gmail.com>
2020-10-21 18:43:36 -0700

    Initial commit for extension

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+
+setup(
+    name='grow-ext-mobile-app-badges',
+    version='1.0.0',
+    license='MIT',
+    author='Grow SDK Authors',
+    author_email='hello@grow.io',
+    package_data={
+        'grow_mobile_app_badges': ['*.yaml'],
+    },
+    packages=[
+        'grow_mobile_app_badges',
+    ],
+)
